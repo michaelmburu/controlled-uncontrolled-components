5178f14ea74006efbf1c5999183597a29791ee8c
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-23 13:39:59 +0300

    - age > 62 exsmple

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const StepOne = ({goToNext}) => (
 const StepTwo = ({goToNext}) => (
   <>
     <h1>Step 2</h1>
-    <button onClick={() => goToNext({age: 20})}>Next</button>
+    <button onClick={() => goToNext({age: 100})}>Next</button>
   </>
 )
 
